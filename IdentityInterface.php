11876ab7caa69596d5bf68dbf57a49b326cf50e4
MSGroupFM <admin@msgru.com>
2022-05-04 00:33:26 +0300

    Add State and relation methods

--- IdentityInterface.php
+++ IdentityInterface.php
@@ -7,4 +7,7 @@ interface IdentityInterface
     public function getId(): int;
     public function load(int $id): void;
     public function getPermissions(): array;
+    public function getState(string $key, $default);
+    public function getStateFromRequest(string $key, $request, $default, $filter);
+    public function setState(string $key, $value): void;
 }
