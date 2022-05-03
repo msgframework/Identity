026c3add3667a30c781dce316e6f39f8a4384fa9
MSGroupFM <admin@msgru.com>
2022-05-04 00:48:15 +0300

    Change parameter type in getStateFromRequest

--- IdentityInterface.php
+++ IdentityInterface.php
@@ -8,6 +8,6 @@ interface IdentityInterface
     public function load(int $id): void;
     public function getPermissions(): array;
     public function getState(string $key, $default);
-    public function getStateFromRequest(string $key, string $request, $default, $filter);
+    public function getStateFromRequest(string $key, string $request, $default, int $filter = \FILTER_DEFAULT, $options = array());
     public function setState(string $key, $value): void;
 }
